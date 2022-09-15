d59fca6b5e0f77edf9d8d628d71d0d92202d1519
Alyona <mirka-sobaka@inbox.ru>
2022-09-15 14:01:28 +0400

    добавила цикл if else в папку example005

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "алена"
+    )
+{
+    Console.WriteLine("Ух, наконец это ты, Алена, УРА!");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
